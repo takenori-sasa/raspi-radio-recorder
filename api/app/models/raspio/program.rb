54227adeab85aa09ca2ab11401464d21dad6991c
takenori-sasa <yoshida.tkw@gmail.com>
2022-08-16 06:04:03 +0900

    🚿#11 URL validationのgem削除

--- api/app/models/raspio/program.rb
+++ api/app/models/raspio/program.rb
@@ -2,7 +2,7 @@ class Raspio::Program < ApplicationRecord
   belongs_to :raspio_station, class_name: 'Raspio::Station'
   validates :title, presence: true
   validates :from, presence: true
-  validates :homepage, url: { allow_nil: true }
+  validates :homepage, format: /\A#{URI::DEFAULT_PARSER.make_regexp(['http', 'https'])}\z/, allow_blank: true
   validates :to, presence: true
   validate :from_lt_to
   def station
--- api/app/models/raspio/station.rb
+++ api/app/models/raspio/station.rb
@@ -2,7 +2,7 @@ module Raspio
   class Station < ApplicationRecord
     validates :id, presence: true
     validates :name, presence: true
-    validates :banner, url: { allow_blank: true }
+    validates :banner, format: /\A#{URI::DEFAULT_PARSER.make_regexp(['http', 'https'])}\z/, allow_blank: true
     has_many  :raspio_programs, class_name: 'Raspio::Program', foreign_key: :raspio_station_id, dependent: :destroy, inverse_of: :raspio_station
     def programs
       self.raspio_programs
