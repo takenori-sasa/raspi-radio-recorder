3cd551a9abc91cb7a1caaf572ef7934590c70201
takenori-sasa <yoshida.tkw@gmail.com>
2022-08-16 06:01:09 +0900

    👍#11 ちょびっとvalidation追加

--- api/app/models/raspio/program.rb
+++ api/app/models/raspio/program.rb
@@ -4,7 +4,15 @@ class Raspio::Program < ApplicationRecord
   validates :from, presence: true
   validates :homepage, url: { allow_nil: true }
   validates :to, presence: true
+  validate :from_lt_to
   def station
     self.raspio_station
+  end
+
+  private
+
+  def from_lt_to
+    # return unless self.from < self.to
+    # @todo errorに追加する
   end
 end
