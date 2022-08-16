d0fb7506f45452882d639508eb96a5939a34b75c
takenori-sasa <yoshida.tkw@gmail.com>
2022-08-17 00:10:33 +0900

    ♻️ #17 recordを整備した

--- api/app/controllers/api/v1/records_controller.rb
+++ api/app/controllers/api/v1/records_controller.rb
@@ -1,12 +1,9 @@
-require 'uri'
-require 'net/http'
-require 'base64'
 require 'tempfile'
+require 'pry'
 module Api
   module V1
     class RecordsController < ApplicationController
       include Raspio
-      before_action :authorization, only: [:create]
       before_action :set_record, only: [:show, :update, :destroy]
 
       def index
@@ -20,9 +17,10 @@ module Api
 
       def create
         record = Raspio::Record.new(ffmpeg_params)
+        record.authorize
         Tempfile.open([record.title, ".aac"]) do |file|
           file.binmode
-          result = download_audio(file, record)
+          result = record.append(file)
           render json: { status: 'ERROR', data: 'download failed.' } unless result
 
           record.audio.attach(io: file, filename: "#{record.title}.aac", content_type: "audio/aac")
@@ -58,57 +56,11 @@ module Api
       end
 
       def ffmpeg_params
-        pparams = params.require(:record).permit(:station_id, :from,
-                                                 :to)
+        pparams = params.require(:record)
+                        .permit(:station_id, :from,
+                                :to)
         title = "#{params[:record][:station_id]}_#{params[:record][:from]}_#{params[:record][:to]}"
         pparams.merge(title:)
-      end
-
-      def authorization
-        res1 = authorization1
-        render json: { status: 'ERROR', data: 'authorization1 failed.' } and return unless res1.is_a?(Net::HTTPSuccess)
-
-        res2 = authorization2(res1)
-        render json: { status: 'ERROR', data: 'authorization2 failed.' } and return unless res2.is_a?(Net::HTTPSuccess)
-
-        @auth_token = res1['X-Radiko-AuthToken']
-      end
-
-      def download_audio(file, record)
-        station_id = record.station_id
-        from = record.from
-        to = record.to
-        system("ffmpeg -headers \"X-Radiko-AuthToken:#{@auth_token}\" -i \"https://radiko.jp/v2/api/ts/playlist.m3u8?station_id=#{station_id}&l=15&ft=#{from}00&to=#{to}00\" -loglevel \"error\" -vn -y -acodec copy #{file.path}")
-      end
-
-      def authorization1
-        uri = URI.parse('https://radiko.jp/v2/api/auth1')
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = uri.scheme == "https"
-
-        autho1_headers = { 'X-Radiko-App': 'pc_html5',
-                           'X-Radiko-App-Version': '0.0.1',
-                           'X-Radiko-User': 'dummy_user',
-                           'X-Radiko-Device': 'pc' }
-        http.get(uri.path, autho1_headers)
-      end
-
-      def authorization2(res)
-        auth_token = res['X-Radiko-AuthToken']
-        key_length = res['X-Radiko-KeyLength'].to_i
-        key_offset = res['X-Radiko-KeyOffset'].to_i
-        autho2_headers = { 'X-Radiko-AuthToken': auth_token,
-                           'X-Radiko-PartialKey': partial_key(Record::AUTHKEY, key_length, key_offset),
-                           'X-Radiko-User': 'dummy_user',
-                           'X-Radiko-Device': 'pc' }
-        uri = URI.parse('https://radiko.jp/v2/api/auth2')
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = uri.scheme == "https"
-        http.get(uri.path, autho2_headers)
-      end
-
-      def partial_key(key, length, offset)
-        Base64.encode64(key.slice(offset, length))
       end
     end
   end
--- api/app/models/raspio/record.rb
+++ api/app/models/raspio/record.rb
@@ -1,7 +1,10 @@
+require 'uri'
+require 'net/http'
+require 'base64'
 module Raspio
   class Record < ApplicationRecord
     validates :title, presence: true
-    attr_accessor :from, :to, :station_id
+    attr_accessor :from, :to, :station_id, :auth_token
 
     # validate :params
 
@@ -9,5 +12,55 @@ module Raspio
     # @see https://radiko.jp/apps/js/playerCommon.js
     # @see https://blog.bluedeer.net/archives/224
     AUTHKEY = 'bcd151073c03b352e1ef2fd66c32209da9ca0afa'.freeze
+    def append(file)
+      station_id = self.station_id
+      from = self.from
+      to = self.to
+      system("ffmpeg -headers \"X-Radiko-AuthToken:#{self.auth_token}\" -i \"https://radiko.jp/v2/api/ts/playlist.m3u8?station_id=#{station_id}&l=15&ft=#{from}00&to=#{to}00\" -loglevel \"error\" -vn -y -acodec copy #{file.path}")
+    end
+
+    def authorize
+      res1 = Authorizer.authorization1
+      render json: { status: 'ERROR', data: 'authorization1 failed.' } and return unless res1.is_a?(Net::HTTPSuccess)
+
+      res2 = Authorizer.authorization2(res1)
+      render json: { status: 'ERROR', data: 'authorization2 failed.' } and return unless res2.is_a?(Net::HTTPSuccess)
+
+      self.auth_token = res1['X-Radiko-AuthToken']
+    end
+    concerning :Authorizer do
+      extend ActiveSupport::Concern
+      extend self
+
+      def authorization1
+        uri = URI.parse('https://radiko.jp/v2/api/auth1')
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = uri.scheme == "https"
+
+        autho1_headers = { 'X-Radiko-App': 'pc_html5',
+                           'X-Radiko-App-Version': '0.0.1',
+                           'X-Radiko-User': 'dummy_user',
+                           'X-Radiko-Device': 'pc' }
+        http.get(uri.path, autho1_headers)
+      end
+
+      def authorization2(res)
+        auth_token = res['X-Radiko-AuthToken']
+        key_length = res['X-Radiko-KeyLength'].to_i
+        key_offset = res['X-Radiko-KeyOffset'].to_i
+        autho2_headers = { 'X-Radiko-AuthToken': auth_token,
+                           'X-Radiko-PartialKey': partial_key(Record::AUTHKEY, key_length, key_offset),
+                           'X-Radiko-User': 'dummy_user',
+                           'X-Radiko-Device': 'pc' }
+        uri = URI.parse('https://radiko.jp/v2/api/auth2')
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = uri.scheme == "https"
+        http.get(uri.path, autho2_headers)
+      end
+
+      def partial_key(key, length, offset)
+        Base64.encode64(key.slice(offset, length))
+      end
+    end
   end
 end
