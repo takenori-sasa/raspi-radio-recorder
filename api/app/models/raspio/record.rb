2fe436c266b2cea02628c346135f6534863a4534
takenori-sasa <yoshida.tkw@gmail.com>
2022-08-16 18:11:16 +0900

    👍#17 色々追加

--- api/app/models/raspio/record.rb
+++ api/app/models/raspio/record.rb
@@ -6,6 +6,8 @@ module Raspio
     # validate :params
 
     has_one_attached :audio
+    # @see https://radiko.jp/apps/js/playerCommon.js
+    # @see https://blog.bluedeer.net/archives/224
     AUTHKEY = 'bcd151073c03b352e1ef2fd66c32209da9ca0afa'.freeze
   end
 end
