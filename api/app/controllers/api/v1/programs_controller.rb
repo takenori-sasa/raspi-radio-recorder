aecd4f8d6b40e266b7fead818797b587c62d9c9c
takenori-sasa <yoshida.tkw@gmail.com>
2022-08-16 14:59:49 +0900

    👍#11 一日分はできた

--- api/app/controllers/api/v1/programs_controller.rb
+++ api/app/controllers/api/v1/programs_controller.rb
@@ -3,23 +3,24 @@ module Api
     class ProgramsController < ApplicationController
       include Raspio
       before_action :set_program, only: [:show, :update, :destroy]
+      # before_action :make_time_table, only: [:show, :index]
 
-      def index
+      def index(date = [Time.zone.today])
+        create(date) unless Program.find_by(date:)
         programs = Program.order(created_at: :desc)
         render json: { status: 'SUCCESS', message: 'Loaded programs', data: programs }
       end
 
-      def show
+      def show(_date = [Time.zone.today])
         render json: { status: 'SUCCESS', message: 'Loaded the program', data: @program }
       end
 
-      def create
-        program = Program.new(program_params)
-        if program.save
-          render json: { status: 'SUCCESS', data: program }
-        else
-          render json: { status: 'ERROR', data: program.errors }
-        end
+      def create(date)
+        Program::TimeTable.add(date)
+      rescue StandardError => e
+        logger.error(e)
+        render json: { status: 'ERROR', data: e }
+        raise ActiveRecord::Rollback
       end
 
       def destroy
@@ -42,7 +43,7 @@ module Api
       end
 
       def program_params
-        params.require(:program).permit(:title, :date)
+        params.require(:program).permit(:title, date: [Time.zone.today])
       end
     end
   end
--- api/app/models/raspio/program.rb
+++ api/app/models/raspio/program.rb
@@ -1,3 +1,6 @@
+require 'rexml/document'
+require 'net/http'
+require 'nkf'
 class Raspio::Program < ApplicationRecord
   belongs_to :raspio_station, class_name: 'Raspio::Station'
   validates :title, presence: true
@@ -5,7 +8,52 @@ class Raspio::Program < ApplicationRecord
   validates :from, presence: true, comparison: { less_than: :to }
   validates :homepage, format: /\A#{URI::DEFAULT_PARSER.make_regexp(['http', 'https'])}\z/, allow_blank: true
   validates :to, presence: true, comparison: { greater_than: :from }
+  AREA_ID = 'JP26'.freeze
   def station
     self.raspio_station
+  end
+  concerning :TimeTable do
+    extend ActiveSupport::Concern
+    extend self
+    def add(dates)
+      dates.each do |date|
+        add_time_table(date)
+      end
+    end
+
+    private
+
+    def add_time_table(date)
+      uri = URI.parse("https://radiko.jp/v3/program/date/#{date.strftime('%Y%m%d')}/#{AREA_ID}.xml")
+      xml = Net::HTTP.get(uri)
+      doc = REXML::Document.new(xml)
+      REXML::XPath.match(doc, 'radiko/stations/station').map do |station|
+        station_id = station.attributes['id']
+        date = Date.strptime(station.elements['progs/date'].text, '%Y%m%d')
+        REXML::XPath.match(station, 'progs/prog').map do |program|
+          id = program.attributes['id']
+          ft = program.attributes['ft'] + '+09:00'
+          from = Time.strptime(ft, '%Y%m%d%H%M%S%Z')
+          to = Time.strptime(program.attributes['to'] + '+09:00', '%Y%m%d%H%M%S%Z')
+          title = hankaku(program.elements['title'].text)
+          description = descript(program)
+          homepage = program.elements['url'].text
+          prog = Raspio::Program.find_or_initialize_by(id:)
+          prog.update(raspio_station_id: station_id, title:, from:, to:, description:, homepage:, date:)
+          prog.save
+        end
+      end
+    end
+
+    def hankaku(text)
+      NKF.nkf('-W -w -Z1', text).strip
+    end
+
+    def descript(program)
+      concat = "#{program.elements['desc'].text}#{program.elements['info'].text}"
+      stripped = ActionController::Base.helpers.strip_tags(concat).gsub(/&gt;|&lt/, "&gt;" => ">", "&lt;" => "<").gsub(/\t+|\n+/, "\n")
+
+      hankaku(stripped)
+    end
   end
 end
