79d624f6988bfcbb18ce4f09b3a606443d682491
takenori-sasa <yoshida.tkw@gmail.com>
2022-08-19 19:58:56 +0900

    👍# REDIS_URL

--- api/app/sidekiq/raspio/program_job.rb
+++ api/app/sidekiq/raspio/program_job.rb
@@ -2,24 +2,23 @@ class Raspio::ProgramJob
   include Sidekiq::Job
   sidekiq_options retry: 5
   sidekiq_options queue: 'low'
-  sidekiq_retries_exhausted do |msg, _ex|
+  sidekiq_retries_exhausted do |msg, exception|
     Rails.logger.error(msg)
     Rails.logger.error(exception)
     # ExceptionNotifier.call(msg, ex)
   end
 
-  def perform(dates_str)
+  def perform(date_str)
     # params
     # dates_str [string]
-    dates_str.each do |date_str|
-      Raspio::Program.add(date_str)
-    end
+    Raspio::Program.add(date_str)
   end
 end
 # Test code
 # today = Time.zone.today
 # dates = (today - 3...today + 3).to_a
-# dates.map! do |date|
-#   date.strftime("%Y%m%d")
+# dates.each do |date|
+#   date_str = date.strftime("%Y%m%d")
+#   p date_str
+#   Raspio::ProgramJob.perform_async(date_str)
 # end
-# Raspio::ProgramJob.perform_async(dates)
