2e652f9d25da752be5c8698cfa16647f1db28b89
takenori-sasa <yoshida.tkw@gmail.com>
2022-08-19 23:50:52 +0900

    ♻️ #22 loggingおやすみ

--- api/config/initializers/sidekiq.rb
+++ api/config/initializers/sidekiq.rb
@@ -1,7 +1,11 @@
+require 'yaml'
+sidekiq_config = YAML.load_file('config/sidekiq.yml')
+# Sidekiq.logger = ::Logger.new(sidekiq_config[:logfile])
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL", "redis://localhost:6379/1") }
+  config.redis = { url: ENV.fetch("SIDEKIQ_REDIS_URL", "redis://localhost:6379/1") }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL", "redis://localhost:6379/1") }
+  config.redis = { url: ENV.fetch("SIDEKIQ_REDIS_URL", "redis://localhost:6379/1") }
+  # config.logger = ::Logger.new(sidekiq_config[:logfile])
 end
